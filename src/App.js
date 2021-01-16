e42c8ef147c190e103ada96def5d58ee4cccaf1a
99Atharva <atharvatilvankar@gmai.com>
2021-01-16 20:12:04 +0530

    Adding Router

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import Body from "./Body";
+import Services from './Services';
+import { BrowserRouter as Router,Switch , Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Body />
-      <Footer />
+     <Router>
+     <Navigation />
+      <Route path="/body" component={Body} />
+      <Route path="/services" component={Services} />
+     </Router>
+     <Footer/>
     </div>
   );
 }
