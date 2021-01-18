bf3ae40bb181767d63aa1f9ac5a0e44ddf4badb7
99Atharva <atharvatilvankar@gmai.com>
2021-01-18 16:09:22 +0530

    Contact Page

--- src/Elements/Contact.js
+++ src/Elements/Contact.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
+import TextField from '@material-ui/core/TextField';
+import PhoneIcon from '@material-ui/icons/Phone';
+import EmailIcon from '@material-ui/icons/Email';
+import Button from '@material-ui/core/Button';
 
 function Contact() {
     return (
-        <div>
-            <h1>Contact Us</h1>
+        <div className="head"><h2>Contact</h2>
+        <div className="form" >
+            
+            <div className="feedback">
+            <div className="fields">
+            <TextField required id="standard-required" 
+            label="Name" type="Name"  variant="outlined" />
+            </div>
+            
+            <div className="fields">
+            <TextField required id="standard-required" 
+            label="Email"  variant="outlined" />
+            </div>
+
+            <div className="fields">
+            <TextField  id="standard" 
+            label="Phone"  variant="outlined" />
+            </div>
+
+            <div className="fields">
+            <TextField className="messagebox"
+            id="outlined-multiline-static"
+            label="Tell us your problem"
+            multiline
+            rows={3}
+            fullWidth
+            variant="outlined"
+            />
+            
+            </div>
+           <Button  variant="outlined"  >Submit</Button>
+            </div>
+            <div className="contact">
+                
+                <div className="phone">
+                   <PhoneIcon fontSize="large" color="action" /> 
+                   <h4>7878xxxx </h4> 
+                  </div>
+                   <div className="email">
+                   <EmailIcon fontSize="large"  color="error" />
+                   <h4>xyz@gmail.com </h4> 
+                </div>
+
+            </div>
+        </div>
         </div>
     )
 }
