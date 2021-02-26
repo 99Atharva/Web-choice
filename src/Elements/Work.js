1177df1449e6bf98cd76d988cfc22433d9486d57
99Atharva <atharvatilvankar@gmai.com>
2021-02-26 23:47:53 +0530

    Card display

--- src/Elements/Work.js
+++ src/Elements/Work.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Card from "../Components/Card";
 
 function Work() {
     return (
         <div>
            <h1>Work</h1> 
-           <card />
+           <Card />
         </div>
     )
 }
