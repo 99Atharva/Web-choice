a8c0dc2dff526fc06fa777df5e8af985f3884bfd
akshat <AKSHATKASHYAB64@GMAIL.COM>
2021-03-04 02:08:44 +0530

    hi

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 
-var firebaseConfig = {
+const  firebaseApp = {
   apiKey: "AIzaSyB1eok-mfLnxrRYcoJJ2yC8JxvLQ2LUmQk",
   authDomain: "webchoice-backend.firebaseapp.com",
   projectId: "webchoice-backend",
@@ -11,11 +11,11 @@ var firebaseConfig = {
   appId: "1:710505958643:web:2addc526c2c8886634bb86"
 };
 
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseApp);
 
    const auth = firebase.auth();
    const provider = new firebase.auth.GoogleAuthProvider();
-   const db = firebase.firestore();
+   const db = firebaseApp.firestore();
    const storage = firebase.storage();
 
    export {db , auth , provider , storage};
