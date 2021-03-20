76fc91e35863032cb320847baf5db5f03d3aad16
99Atharva <atharvatilvankar@gmai.com>
2021-03-20 18:25:27 +0530

    About Page

--- src/Elements/About.js
+++ src/Elements/About.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import "./About.css";
 
 function About() {
     return (
-        <div>
-            <h1>About</h1>
+        <div className="sec">
+            <h1>About Us</h1>
+            <div className="container">
+            <h2>Who are we ?</h2>
+            <p>Web-choice is social media marketing company which 
+                happens to be run by highly professional  Creative 
+                and .......( we know you aren't going to read 
+                this ), so lets's get straight to the point 😜
+                <br/><br/>
+                We are bunch of amatuers helping
+                 people promote their businesses in the most 
+                 creative and unique ways . The idea behind this was
+                  simple — to help early-stage and growth-stage 
+                  startups leverage the enormous potential of
+                   digital marketing to generate more traffic and 
+                   leads for their business.We exacly know what 
+                   sells on social media , From Memes to Trends , we have done it all .
+                </p>               
+            </div>
+            <div className="tag_line">We Are The Musketeers Of Digital Media <br/>😎</div>
+            
         </div>
     )
 }
